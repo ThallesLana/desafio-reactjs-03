0a31494e518bd060886fec4214f7889692c414fe
ThallesLana <thalles.lana1@gmail.com>
2022-04-10 00:26:50 -0300

    iniciando desafio, header já foi concluido!

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -25,9 +25,12 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
     // const storagedCart = Buscar dados do localStorage
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+   // const storagedCart = localStorage.getItem('@RocketShoes:cart');
+
+    /* if (storagedCart) {
+       return JSON.parse(storagedCart);
+    }
+    */
 
     return [];
   });
